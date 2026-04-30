a88f39ac60495fb752ba0a22a1a18461e3047f6d
Muhammad Afiq <muhammad.afiq_mystep@moh.gov.my>
2026-04-30 10:22:53 +0800

    change visitors/index.blade.php layout

--- resources/views/visitors/index.blade.php
+++ resources/views/visitors/index.blade.php
@@ -1,7 +1,34 @@
-<h1>Visitors Index</h1>
+@extends('layouts.app')
 
-@foreach ($visitors as $visitor)
-    <p>{{ $visitor->name }}</p>
-    <p>{{ $visitor->phone }}</p>
-    <p>{{ $visitor->email }}</p>
-@endforeach
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Visitor Index') }}</div>
+
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th>Name</th>
+                                <th>Phone</th>
+                                <th>Email</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($visitors as $visitor)
+                                <tr>
+                                    <td>{{ $visitor->name }}</td>
+                                    <td>{{ $visitor->phone }}</td>
+                                    <td>{{ $visitor->email }}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
