692fb03e88b6348f14fdddcd9e3c4334e9d5b45e
Muhammad Afiq <muhammad.afiq_mystep@moh.gov.my>
2026-05-14 12:20:46 +0800

    Add authentication logging feature with migration, configuration, and email notifications

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,13 +9,14 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Yadahan\AuthenticationLog\AuthenticationLogable;
 
 #[Fillable(['name', 'email', 'password'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, AuthenticationLogable;
 
     /**
      * Get the attributes that should be cast.
